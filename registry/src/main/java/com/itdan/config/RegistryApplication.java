eb00f1519f2963135a5b17be5b6f429b32f023b8
18376108492 <2207161187@qq.com>
2020-06-29 22:30:30 +0800

    Initial commit

---
+++ registry/src/main/java/com/itdan/config/RegistryApplication.java
@@ -0,0 +1,16 @@
+package com.itdan.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心服务
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
